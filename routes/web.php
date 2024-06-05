7bea3c4fb9ec8db2c68361a42b36234b3decb59f
kazburrel <oegbosionu@gmail.com>
2024-06-05 11:33:03 +0100

    implementation of open ai

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use OpenAI\Laravel\Facades\OpenAI;
 
 Route::view('/', 'dashboard')->name('dashboard');
 
@@ -11,3 +11,12 @@ Route::view('/', 'dashboard')->name('dashboard');
 //     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 //     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 // });
+
+Route::get('/ai', function () {
+    return OpenAI::chat()->create([
+        'model' => 'gpt-3.5-turbo',
+        'messages' => [
+            ['role' => 'user', 'content' => 'Hello!'],
+        ],
+    ]);
+});
